e48826d37e62a46b1452570b13ffedce077781ae
Adernild <alexanderdernild@live.dk>
2020-10-09 04:20:35 +0200

    Dehyrdrated twitter data and minor tweaks

--- data_cleaning.R
+++ data_cleaning.R
@@ -14,117 +14,120 @@ clean_tweets <- function(x) {
     str_trim("both") 
 }
 
+# Variables of interest
+voi <- c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count)
+
 # Deleting variables that are not going to be used
 # Recent 24_hour
 df <- readRDS("data/24_hour/recent/pres_debate.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
+  select(voi)
   
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/24_hour/recent/pres_debate.rds")
 
 df <- readRDS("data/24_hour/recent/trump.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/24_hour/recent/trump.rds")
 
 df <- readRDS("data/24_hour/recent/biden.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count)) 
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/24_hour/recent/biden.rds")
 
 # Mixed 24_hour
 df <- readRDS("data/24_hour/mixed/pres_debate.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
-  
+  select(voi)
+
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/24_hour/mixed/pres_debate.rds")
 
 df <- readRDS("data/24_hour/mixed/trump.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/24_hour/mixed/trump.rds")
 
 df <- readRDS("data/24_hour/mixed/biden.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count)) 
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/24_hour/mixed/biden.rds")
 
 # Popular 24_hour
 df <- readRDS("data/24_hour/popular/pres_debate.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/24_hour/popular/pres_debate.rds")
 
 df <- readRDS("data/24_hour/popular/trump.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/24_hour/popular/trump.rds")
 
 df <- readRDS("data/24_hour/popular/biden.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/24_hour/popular/biden.rds")
 
 # Recent 7_day
 df <- readRDS("data/7_day/recent/pres_debate.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/7_day/recent/pres_debate.rds")
 
 df <- readRDS("data/7_day/recent/trump.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/7_day/recent/trump.rds")
 
 df <- readRDS("data/7_day/recent/biden.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
-  
+  select(voi)
+
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/7_day/recent/biden.rds")
 
 # Mixed 7_day
 df <- readRDS("data/7_day/mixed/pres_debate.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count)) 
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/7_day/mixed/pres_debate.rds")
 
 df <- readRDS("data/7_day/mixed/trump.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
-  
+  select(voi)
+
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/7_day/mixed/trump.rds")
 
 df <- readRDS("data/7_day/mixed/biden.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
-  
+  select(voi)
+
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/7_day/mixed/biden.rds")
 
 # Popular 7_day
 df <- readRDS("data/7_day/popular/pres_debate.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/7_day/popular/pres_debate.rds")
 
 df <- readRDS("data/7_day/popular/trump.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/7_day/popular/trump.rds")
 
 df <- readRDS("data/7_day/popular/biden.rds") %>%
-  select(c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count))
+  select(voi)
 
 df$text <- clean_tweets(df$text)
 saveRDS(df, "data/7_day/popular/biden.rds")

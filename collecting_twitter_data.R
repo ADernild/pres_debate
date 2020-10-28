b3f9832d95b346c8384bb07752189ca5bc557e32
ADernild <alexanderdernild@live.dk>
2020-10-28 17:00:37 +0100

    Adding programmatic methods to data_cleaning.R and collecting_twitter_data.R

--- collecting_twitter_data.R
+++ collecting_twitter_data.R
@@ -1,7 +1,10 @@
 library(rtweet)
 
-key <- 'XXXXXXXXXXXXXXXXXXX' 
-secret <- 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+key <- 'XXXXXXXXXXXXXXXXX'
+secret <- 'XXXXXXXXXXXXXXXXXX'
+
+key <- '0amKFy15EwmUXWpznIRhmS3fF'
+secret <- 'eu8wun4gNoOlSAr5VYVWT0gnRYHKT28U60fCZuqQZgmw70xiPY'
 
 # Connecting to Twitter api
 twitter_token <- create_token(
@@ -21,30 +24,14 @@ twitter_token <- create_token(
 #' file: name and path for rds file to be saved
 #' ---
 
-twitter_grab_recent <- function(word, n, since, until, file) { # Grabbing tweets of recent type
+twitter_grab <- function(word, n, since, until, type, file) { # Grabbing tweets of recent type
   tweets <- search_tweets(q = word,
                           n = n,
                           include_rts = FALSE,
                           lang = "en",
                           since = since,
                           until = until,
-                          type = "recent",
-                          retryonratelimit = TRUE
-  )
-
-  saveRDS(tweets, file = file)
-  cat(file, "saved")
-}
-
-
-twitter_grab_mixed <- function(word, n, since, until, file) { # Grabbing tweets of mixed type
-  tweets <- search_tweets(q = word,
-                          n = n,
-                          include_rts = FALSE,
-                          lang = "en",
-                          since = since,
-                          until = until,
-                          type = "mixed",
+                          type = type,
                           retryonratelimit = TRUE
   )
   
@@ -52,160 +39,20 @@ twitter_grab_mixed <- function(word, n, since, until, file) { # Grabbing tweets 
   cat(file, "saved")
 }
 
-twitter_grab_popular <- function(word, n, since, until, file) { # Grabbing tweets of popular type
-  tweets <- search_tweets(q = word,
-                          n = n,
-                          include_rts = FALSE,
-                          lang = "en",
-                          since = since,
-                          until = until,
-                          type = "popular",
-                          retryonratelimit = TRUE
+# Grabbing tweets programmatically
+ref <- read.csv('twitter_data.csv', stringsAsFactors = FALSE) # .csv file containing paths and search specs
+
+# Looping throug data.frame ref calling twitter_grab() function
+for (row in 40:nrow(ref)) {
+  twitter_grab(
+    ref$term[row],
+    ref$n[row],
+    ref$since[row],
+    ref$until[row],
+    ref$type[row],
+    ref$path[row]
   )
-  
-  saveRDS(tweets, file = file)
-  cat(file, "saved")
 }
-
-# 24 Hour datsets:
-
-# Grabbing recent tweets:
-# Creating recent/pres_debate.rds from #presidentialdebate tweets
-twitter_grab_recent(word = "presidentialdebate",
-                    n = 36000,
-                    since = "2020-09-30",
-                    until = "2020-10-01",
-                    file = "data/24_hour/recent/pres_debate.rds")
-
-# Creating recent/trump.rds from #trump tweets
-twitter_grab_recent(word = "trump",
-                    n = 18000,
-                    since = "2020-09-30",
-                    until = "2020-10-01",
-                    file = "data/24_hour/recent/trump.rds")
-
-# Creating recent/biden.rds from #biden tweets
-twitter_grab_recent(word = "biden",
-                    n = 18000,
-                    since = "2020-09-30",
-                    until = "2020-10-01",
-                    file = "data/24_hour/recent/biden.rds")
-
-# Grabbing mixed tweets:
-# Creating mixed/pres_debate.rds from #presidentialdebate tweets
-twitter_grab_mixed(word = "presidentialdebate",
-                   n = 36000,
-                   since = "2020-09-30",
-                   until = "2020-10-01",
-                   file = "data/24_hour/mixed/pres_debate.rds")
-
-# Creating mixed/trump.rds from #trump tweets
-twitter_grab_mixed(word = "trump",
-                   n = 18000,
-                   since = "2020-09-30",
-                   until = "2020-10-01",
-                   file = "data/24_hour/mixed/trump.rds")
-
-# Creating mixed/biden.rds from #biden tweets
-twitter_grab_mixed(word = "biden",
-                   n = 18000,
-                   since = "2020-09-30",
-                   until = "2020-10-01",
-                   file = "data/24_hour/mixed/biden.rds")
-
-# Grabbing popular tweets:
-# Creating popular/pres_debate.rds from #presidentialdebate tweets
-twitter_grab_popular(word = "presidentialdebate",
-                     n = 36000,
-                     since = "2020-09-30",
-                     until = "2020-10-01",
-                     file = "data/24_hour/popular/pres_debate.rds")
-
-# Creating popular/trump.rds from #trump tweets
-twitter_grab_popular(word = "trump",
-                     n = 18000,
-                     since = "2020-09-30",
-                     until = "2020-10-01",
-                     file = "data/24_hour/popular/trump.rds")
-
-# Creating popular/biden.rds from #biden tweets
-twitter_grab_popular(word = "biden",
-                     n = 18000,
-                     since = "2020-09-30",
-                     until = "2020-10-01",
-                     file = "data/24_hour/popular/biden.rds")
-
-# 7 day datasets:
-
-# Grabbing recent tweets:
-# Creating recent/pres_debate.rds from #presidentialdebate tweets
-twitter_grab_recent(word = "presidentialdebate",
-                    n = 36000,
-                    since = "2020-09-27",
-                    until = "2020-10-04",
-                    file = "data/7_day/recent/pres_debate.rds")
-
-# Creating recent/trump.rds from #trump tweets
-twitter_grab_recent(word = "trump",
-                    n = 18000,
-                    since = "2020-09-27",
-                    until = "2020-10-04",
-                    file = "data/7_day/recent/trump.rds")
-
-# Creating recent/biden.rds from #biden tweets
-twitter_grab_recent(word = "biden",
-                    n = 18000,
-                    since = "2020-09-27",
-                    until = "2020-10-04",
-                    file = "data/7_day/recent/biden.rds")
-
-# Grabbing mixed tweets:
-# Creating mixed/pres_debate.rds from #presidentialdebate tweets
-twitter_grab_mixed(word = "presidentialdebate",
-                   n = 36000,
-                   since = "2020-09-27",
-                   until = "2020-10-04",
-                   file = "data/7_day/mixed/pres_debate.rds")
-
-# Creating mixed/trump.rds from #trump tweets
-twitter_grab_mixed(word = "trump",
-                   n = 18000,
-                   since = "2020-09-27",
-                   until = "2020-10-04",
-                   file = "data/7_day/mixed/trump.rds")
-
-# Creating mixed/biden.rds from #biden tweets
-twitter_grab_mixed(word = "biden",
-                   n = 18000,
-                   since = "2020-09-27",
-                   until = "2020-10-04",
-                   file = "data/7_day/mixed/biden.rds")
-
-# Grabbing popular tweets:
-# Creating popular/pres_debate.rds from #presidentialdebate tweets
-twitter_grab_popular(word = "presidentialdebate",
-                     n = 36000,
-                     since = "2020-09-27",
-                     until = "2020-10-04",
-                     file = "data/7_day/popular/pres_debate.rds")
-
-# Creating popular/trump.rds from #trump tweets
-twitter_grab_popular(word = "trump",
-                     n = 18000,
-                     since = "2020-09-27",
-                     until = "2020-10-04",
-                     file = "data/7_day/popular/trump.rds")
-
-# Creating popular/biden.rds from #biden tweets
-twitter_grab_popular(word = "biden",
-                     n = 18000,
-                     since = "2020-09-27",
-                     until = "2020-10-04",
-                     file = "data/7_day/popular/biden.rds")
-
-
-
-
 
 
 
--- data_cleaning.R
+++ data_cleaning.R
@@ -14,120 +14,19 @@ clean_tweets <- function(x) {
     str_trim("both") 
 }
 
-# Variables of interest
-voi <- c(created_at, text, hashtags, location, retweet_count, favorite_count, followers_count)
+# Cleaning tweets programmatically
+ref <- read.csv('twitter_data.csv', stringsAsFactors = FALSE) # .csv file containing path and altpath
 
-# Deleting variables that are not going to be used
-# Recent 24_hour
-df <- readRDS("data/24_hour/recent/pres_debate.rds") %>%
-  select(voi)
-  
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/24_hour/recent/pres_debate.rds")
+voi <- c('user_id', 'status_id', 'created_at',
+         'text', 'hashtags', 'location',
+         'retweet_count', 'favorite_count', 'followers_count') # Variables of interest
 
-df <- readRDS("data/24_hour/recent/trump.rds") %>%
-  select(voi)
+# Looping through ref reading in datasets containing tweets and cleaning text variable
+for (row in 1:nrow(ref)) {
+  df <- readRDS(ref$path[row]) %>%
+    select(all_of(voi))
+  df$text <- clean_tweets(df$text)
+  saveRDS(df, ref$altpath[row])
+}
 
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/24_hour/recent/trump.rds")
 
-df <- readRDS("data/24_hour/recent/biden.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/24_hour/recent/biden.rds")
-
-# Mixed 24_hour
-df <- readRDS("data/24_hour/mixed/pres_debate.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/24_hour/mixed/pres_debate.rds")
-
-df <- readRDS("data/24_hour/mixed/trump.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/24_hour/mixed/trump.rds")
-
-df <- readRDS("data/24_hour/mixed/biden.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/24_hour/mixed/biden.rds")
-
-# Popular 24_hour
-df <- readRDS("data/24_hour/popular/pres_debate.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/24_hour/popular/pres_debate.rds")
-
-df <- readRDS("data/24_hour/popular/trump.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/24_hour/popular/trump.rds")
-
-df <- readRDS("data/24_hour/popular/biden.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/24_hour/popular/biden.rds")
-
-# Recent 7_day
-df <- readRDS("data/7_day/recent/pres_debate.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/7_day/recent/pres_debate.rds")
-
-df <- readRDS("data/7_day/recent/trump.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/7_day/recent/trump.rds")
-
-df <- readRDS("data/7_day/recent/biden.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/7_day/recent/biden.rds")
-
-# Mixed 7_day
-df <- readRDS("data/7_day/mixed/pres_debate.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/7_day/mixed/pres_debate.rds")
-
-df <- readRDS("data/7_day/mixed/trump.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/7_day/mixed/trump.rds")
-
-df <- readRDS("data/7_day/mixed/biden.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/7_day/mixed/biden.rds")
-
-# Popular 7_day
-df <- readRDS("data/7_day/popular/pres_debate.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/7_day/popular/pres_debate.rds")
-
-df <- readRDS("data/7_day/popular/trump.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/7_day/popular/trump.rds")
-
-df <- readRDS("data/7_day/popular/biden.rds") %>%
-  select(voi)
-
-df$text <- clean_tweets(df$text)
-saveRDS(df, "data/7_day/popular/biden.rds")
